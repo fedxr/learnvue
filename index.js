5f27aa947762ce0131d5c4fc3dd7090088d534b4
fedxr <you2xr@126.com>
2016-09-13 22:21:13 +0800

    vuejs start

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 new Vue({
 	el: '#app',
 	data: {
-		message: 'Hello Vue.js'
+		message: 'Hello Vue.js',
+		todos: [
+			{text: 'Learn JavaScript'},
+			{text: 'Learn Vue.js'},
+			{text: 'Build Something Awesome'}
+		],
+		newTodo: '',
+		dos: [
+			{text: 'Add some todos'}
+		]
+	},
+	methods: {
+		reverseMessage: function () {
+			this.message = this.message.split('').reverse().join('');
+		},
+		addTodo: function () {
+			var text = this.newTodo.trim();
+			if (text) {
+				this.dos.push({text: text});
+				this.newTodo = '';
+			}
+		},
+		removeTodo: function (index) {
+			this.dos.splice(index, 1);
+		}
 	}
 });
