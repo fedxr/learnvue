eec952f2c0bf2d920cd52d6f5da27473eb1e5f2f
fedxr <you2xr@126.com>
2016-10-09 17:49:06 +0800

    start v2

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
-new Vue({
-	el: '#app',
+var app2 = new Vue({
+	el: '#app-2',
 	data: {
-		message: 'Hello Vue.js',
-		todos: [
-			{text: 'Learn JavaScript'},
-			{text: 'Learn Vue.js'},
-			{text: 'Build Something Awesome'}
-		],
-		newTodo: '',
-		dos: [
-			{text: 'Add some todos'}
-		]
-	},
-	methods: {
-		reverseMessage: function () {
-			this.message = this.message.split('').reverse().join('');
-		},
-		addTodo: function () {
-			var text = this.newTodo.trim();
-			if (text) {
-				this.dos.push({text: text});
-				this.newTodo = '';
-			}
-		},
-		removeTodo: function (index) {
-			this.dos.splice(index, 1);
-		}
+		message: 'You loaded this page on' + new Date()
 	}
 });
