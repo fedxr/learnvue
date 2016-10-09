ee845460d2764728fea543ad33952083c7ec7090
fedxr <you2xr@126.com>
2016-10-09 18:11:41 +0800

    conditionals and loops

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app2 = new Vue({
 	el: '#app-2',
 	data: {
-		message: 'You loaded this page on' + new Date()
+		message: 'You loaded this page on' + new Date(),
+		seen: true,
+		todos: [
+			{ text: 'Learn JavaScript' },
+      		{ text: 'Learn Vue' },
+      		{ text: 'Build something awesome' }
+		]
 	}
 });
