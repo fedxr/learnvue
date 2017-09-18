e04bad47878a98bf97feaa3d1cbd9aa6ae239e47
you2xr@126.com <you2xr@126.com>
2017-09-18 17:55:54 +0800

    learn vue component

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app2 = new Vue({
 	el: '#app-2',
 	data: {
 		message: 'You loaded this page on' + new Date(),
+		message2: 'Hello Vue.js!',
+		message3: 'input something',
 		seen: true,
 		todos: [{
 			text: 'Learn JavaScript'
@@ -10,5 +12,26 @@ var app2 = new Vue({
 		}, {
 			text: 'Build something awesome'
 		}]
+	},
+	methods: {
+		reverseMessage: function() {
+			this.message2 = this.message2.split('').reverse().join('');
+		}
+	}
+});
+
+Vue.component('todo-item',{
+	props: ['todo'],
+	template:'<li>{{todo.text}}</li>'
+});
+
+var app7 = new Vue({
+	el: '#app-7',
+	data: {
+		groceryList:[
+			{ id: 0, text: '蔬菜' },
+			{ id: 1, text: '奶酪' },
+			{ id: 2, text: '随便其他什么人吃的东西' }
+		]
 	}
 });
