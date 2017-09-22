160accc77dad32ecbee7a627d0509481bd197a9d
you2xr@126.com <you2xr@126.com>
2017-09-22 18:07:03 +0800

    display filtered

--- listRendering/index.js
+++ listRendering/index.js
@@ -29,3 +29,27 @@ new Vue({
           }
     }
 });
+
+var exmaple3 = new Vue({
+    el: '#example-3',
+    // data: {
+    //     numbers: [ 1, 2, 3, 4, 5 ]
+    // },
+    // computed: {
+    //     evenNumbers: function() {
+    //         return this.numbers.filter(function(number){
+    //             return number % 2 === 0;
+    //         });
+    //     }
+    // }
+    data: {
+        numbers: [ 1, 2, 3, 4, 5 ]
+      },
+      methods: {
+        even: function (numbers) {
+          return numbers.filter(function (number) {
+            return number % 2 === 0
+          })
+        }
+      }
+});
