8daa20f2a4782ee4152027a56c17082ac2476c87
you2xr@126.com <you2xr@126.com>
2017-09-25 17:03:07 +0800

    event handling

--- eventHandling/index.js
+++ eventHandling/index.js
@@ -1,9 +1,32 @@
 var example1 = new Vue({
-    el:'#exmaple-1',
+    el:'#example-1',
     data: {
-        items: [
-            {message: 'Foo'},
-            {message: 'Bar'}
-        ] 
+        counter:0
+    }
+});
+
+var example2 = new Vue({
+    el:'#example-2',
+    data: {
+        name: 'Vue.js'
+    },
+    methods: {
+        greet: function(event) {
+            // `this` 在方法里指当前 Vue 实例
+            alert('Hello ' + this.name + '!');
+            // `event` 是原生 DOM 事件
+            if(event){
+                alert(event.target.tagName);
+            }
+        }
+    }
+});
+
+var example3 = new Vue({
+    el: '#example-3',
+    methods: {
+        say: function(message){
+            alert(message);
+        }
     }
 });
