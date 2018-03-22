2fe7ade6d398d8649d734f2237cc23b8ff3178ae
you2xr@126.com <you2xr@126.com>
2018-03-22 14:01:04 +0800

    scoped slot

--- components/contentDistributionWithSlots.js
+++ components/contentDistributionWithSlots.js
@@ -1,3 +1,4 @@
+// single slot
 Vue.component('my-component', {
     template: '<div>\
                 <h2>我是子组件的标题</h2>\
@@ -11,8 +12,9 @@ new Vue({
     el: '#singleSlot'
 });
 
+// named slot
 Vue.component('app-layout', {
-    template:'<div class="container">\
+    template: '<div class="container">\
                 <header>\
                     <slot name="header"></slot>\
                 </header>\
@@ -27,4 +29,71 @@ Vue.component('app-layout', {
 
 new Vue({
     el: '#namedSlot'
+});
+
+// scoped slots
+Vue.component('my-awesome-list', {
+    template: '<ul>\
+                <slot name="item"\
+                    v-for="item in items"\
+                    :text="item.text">\
+                </slot>\
+            </ul>',
+    props: ['items']
+})
+new Vue({
+    el: '#app',
+    data: {
+        items: [
+            { text: '逛' },
+            { text: '吃' },
+            { text: '逛' },
+            { text: '吃' },
+            { text: '逛' },
+            { text: '吃' }
+        ]
+    }
+})
+
+// dynamic components
+var Home = {
+    template: '<p>Welcome home!</p>'
+};
+var Posts = {
+    template: '<p>Welcome posts!</p>'
+};
+var Archive = {
+    template: '<p>Welcome archive!</p>'
+};
+
+var vm = new Vue({
+    el: '#keepAlive',
+    data: {
+        currentView: Posts
+    },
+    // components: {
+    //     home: {
+    //         template: '<p>Welcome home!</p>'
+    //     },
+    //     posts: {
+    //         template: '<p>Welcome posts!</p>'
+    //     },
+    //     archive: {
+    //         template: '<p>Welcome archive!</p>'
+    //     }
+    // }
+});
+
+// async components
+Vue.component('async-example', function (resolve, reject) {
+    setTimeout(function () {
+        // 将组件定义传入 resolve 回调函数
+        resolve({
+            template: '<div>I am async</div>'
+        })
+    }, 1000);
+});
+
+new Vue({
+    el: '#async'
 });
