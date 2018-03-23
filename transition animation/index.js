b6705f1866aa1e3146421f61df335d5119ebc2fa
you2xr@126.com <you2xr@126.com>
2018-03-23 09:57:48 +0800

    transtions and animation

---
+++ transition animation/index.js
@@ -0,0 +1,30 @@
+new Vue({
+    el: '#demo',
+    data: {
+        show: true
+    }
+});
+
+// CSS Transitions
+new Vue({
+    el: '#example-1',
+    data: {
+        show: true
+    }
+});
+
+// CSS Animations
+new Vue({
+    el: '#example-2',
+    data: {
+        show: true
+    }
+});
+
+// custom transition classes
+new Vue({
+    el: '#example-3',
+    data: {
+        show: true
+    }
+});
